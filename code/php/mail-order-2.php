b6fab46c9633424e70048edaed243885b2ec0c51
vitorvicente <vitor@bu.edu>
2019-12-31 11:44:19 +0000

    PHP Order Functions Update

--- code/php/mail-order-2.php
+++ code/php/mail-order-2.php
@@ -2,9 +2,9 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $net = $_POST['network'];
-$package = $_POST['package'];
+$request = $_POST['request'];
 $details = $_POST['details'];
-$formcontent=" From: $name \n Network: $net \n Package: $package \n Details: $details";
+$formcontent=" From: $name \n Network: $net \n Request: $request \n Details: $details";
 
 $recipient = "vitor@bu.edu";
 $subject = "Contact Form - VitorVicente.com";
@@ -12,5 +12,5 @@ $mailheader = "From: $email";
 
 
 mail($recipient, $subject, $formcontent, $mailheader);
-header("Location: ../../services/services/minecraft/order/")
+header("Location: ../../services/services/minecraft/other-order/")
 ?>
